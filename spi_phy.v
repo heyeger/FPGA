6a9a10a976c1cd55c07b0d3219fee62d4e91062f
heyeger <40923015+heyeger@users.noreply.github.com>
2023-03-13 11:38:06 +0800

    Update spi_phy.v

--- spi_phy.v
+++ spi_phy.v
@@ -1,9 +1,24 @@
 module spi_phy(
   input  i_clk,
   input  i_rst_n,
+  
+  // apb interface
+  input [15:0]  i_paddr,
+  input         i_pwrite,
+  input         i_psel,
+  input         i_penable,
+  input         i_pwdata
+  
+  output        o_pready,
+  output [15:0] o_prdata,
+  
+  
+  // spi interface
   output o_spi_csn,
   output o_spi_clk,
-  inout  io_spi_sdio
+  output o_spi_ioe,
+  input  i_spi_sdi,
+  output o_spi_sdo
   );
 
 
