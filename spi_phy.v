61dedc0831a561c6589ac7b1df84fba639a00902
heyeger <40923015+heyeger@users.noreply.github.com>
2023-03-13 14:08:40 +0800

    Update spi_phy.v

--- spi_phy.v
+++ spi_phy.v
@@ -3,14 +3,14 @@ module apb_spi_phy(
   input  i_rst_n,
   
   // apb slave interface
-  input [15:0]  i_paddr,
+  input [31:0]  i_paddr,
   input         i_pwrite,
   input         i_psel,
   input         i_penable,
-  input [15:0]  i_pwdata
+  input [31:0]  i_pwdata
   
   output        o_pready,
-  output [15:0] o_prdata,
+  output [31:0] o_prdata,
   output        o_pslverr,
   
   
